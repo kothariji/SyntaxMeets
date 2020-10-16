7075a0d8f33d6d87642722ba166cc858436774d9
Dhruv Kothari <kotharidhruv25@gmail.com>
2020-10-16 21:58:08 +0530

    added undo and clear

--- frontend/syntaxmeets/src/components/ThinkPad.js
+++ frontend/syntaxmeets/src/components/ThinkPad.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { InputLabel, MenuItem, Select, AppBar, Toolbar, Typography, Button } from "@material-ui/core"
-import ShareIcon from '@material-ui/icons/Share';
 import CanvasDraw from "react-canvas-draw";
+import UndoIcon from '@material-ui/icons/Undo';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 class ThinkPad extends Component {
     render() {
@@ -12,19 +13,36 @@ class ThinkPad extends Component {
                         <Typography variant="h5" style={{ 'fontFamily': "poppins", "color": "#f1f3f8" }}>
                             ThinkPad
                     </Typography>
-                        <Button variant="contained" startIcon={<ShareIcon />} style={{ 'fontFamily': "poppins", 'marginLeft': "auto", 'fontWeight': "600", 'color': "#f1f3f8", 'backgroundColor': "#99A3CD" }}>
+                        <Button
+                            variant="contained"
+                            onClick={() => {
+                                this.saveableCanvas.clear();
+                            }}
+                            startIcon={<DeleteIcon />}
+                            style={{ 'fontFamily': "poppins", 'marginLeft': "auto", 'fontWeight': "600", 'color': "#f1f3f8", 'backgroundColor': "#99A3CD" }}
+                        >Clear</Button>
+                        <Button
+                            variant="contained"
+                            startIcon={<UndoIcon />}
+                            style={{ 'fontFamily': "poppins", 'marginLeft': '15px', 'fontWeight': "600", 'color': "#f1f3f8", 'backgroundColor': "#99A3CD" }}
+                            onClick={() => {
+                                this.saveableCanvas.undo();
+                            }}
+                        >UNDO</Button>
 
-                            Share</Button>
                     </Toolbar>
                 </AppBar>
                 <CanvasDraw
+                    ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
                     canvasWidth={690}
                     canvasHeight={548}
-                    brushRadius={6}
+                    brushRadius={3}
                     brushColor={"#393b44"}
                     catenaryColor={"#99A3CD"}
                     gridColor={"#f1f3f8"}
                 />
+
+
             </Fragment>
         );
     }
